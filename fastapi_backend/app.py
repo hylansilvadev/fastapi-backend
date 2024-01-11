d1e6d1b3bc068d4e906993574adc9eec98832b96
Hylan Silva <hylansilva@outlook.com>
2024-01-11 01:24:34 +0000

    make a event crud

--- fastapi_backend/app.py
+++ fastapi_backend/app.py
@@ -1,8 +1,56 @@
-from fastapi import FastAPI
+from typing import List
+
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.get('/')
-async def hello_world() -> str:
-    return 'Hello World... 🌎🌍🌏'
+database = []
+
+
+class Event(BaseModel):
+    name: str
+    description: str
+    quantity_of_tickets: int
+    is_sold: bool = False
+    owner: str
+    date: str
+    expires_sel_date: str
+
+
+@app.post('/event')
+async def create_event(event: Event) -> Event:
+    database.append(event)
+    return event
+
+
+@app.get('/event/{event_id}')
+async def list_a_event(event_id: int) -> Event:
+    if event_id >= len(database):
+        raise HTTPException(status_code=404, detail='Evento nao encontrado :(')
+
+    return database[event_id]
+
+
+@app.get('/events')
+async def list_all_events() -> List[Event]:
+    return database
+
+
+@app.put('/event/{event_id}')
+async def update_event(event_id: int, event: Event) -> List[Event]:
+    if event_id >= len(database):
+        raise HTTPException(status_code=404, detail='Evento nao encontrado :(')
+
+    database[event_id] = event
+    return database
+
+
+@app.delete('/event/{event_id}')
+async def remove_a_event(event_id: int) -> str:
+    if event_id >= len(database):
+        raise HTTPException(status_code=404, detail='evento nao encontrado :(')
+
+    database.pop(event_id)
+    return 'Evento excluído com sucesso!'
